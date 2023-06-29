4c3efc49ff5f3921b4554b4151f2a99d795cb6f9
dominik Pietrzyk <domino1661@gmail.com>
2023-06-29 23:27:47 +0200

    added nice date format

--- src/app/shared/pipes/date-format.pipe.ts
+++ src/app/shared/pipes/date-format.pipe.ts
@@ -1,13 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { formatDistance, subDays } from 'date-fns';
+import { format, formatDistance, formatRelative, subDays } from 'date-fns';
 
 @Pipe({
   name: 'dateFormat'
 })
 export class DateFormatPipe implements PipeTransform {
 
-  transform(date: Date): string {
-    return formatDistance(subDays(date, 3), date, { addSuffix: true })
+  transform(date: Date): unknown {
+    const currentDate = new Date()
+    const yourDate: Date = new Date(new Date(date) + ' UTC');
+
+    // 864000000 is one day in miliseconds
+    if(currentDate.getTime() - yourDate.getTime() > 86400000){
+      return formatRelative(yourDate, new Date())
+    }
+    return format(yourDate, 'HH:mm')
+    
   }
 
 }
