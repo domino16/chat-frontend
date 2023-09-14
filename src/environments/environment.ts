2bb9a075af78326025cf13cdcfb8570506618e4f
dominik Pietrzyk <domino1661@gmail.com>
2023-09-14 12:11:56 +0200

    improved search input

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrlAuth: "http://chat-auth-backend-dev.eu-central-1.elasticbeanstalk.com",
-  apiUrlChat: "http://chat-message-backend-dev.eu-central-1.elasticbeanstalk.com",
-  wsApiUrl: "ws://chat-message-backend-dev.eu-central-1.elasticbeanstalk.com/ws",
+  apiUrlAuth: "https://auth-api.sendm.site",
+  apiUrlChat: "https://message-api.sendm.site",
+  wsApiUrl: "wss://message-api.sendm.site/ws",
 };
